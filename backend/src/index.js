655ea45a90dd863bc75e555af907d267bfd3bbf2
Jhon Lay Lopez <jhonlaylopez000@gmail.com>
2025-08-13 00:27:00 +0800

    add MongoDB connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import connectDB from "./lib/db.js";
+
 dotenv.config();
 
 const app = express();
@@ -10,12 +12,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
 	res.send("Hello World!");
 });
 
-app.listen(port, () => {
-	console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+	connectDB();
+	console.log(`Server running on http://localhost:${PORT}`);
 });
