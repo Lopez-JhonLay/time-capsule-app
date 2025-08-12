f59a2c3084e7cf0df5895ae249341c8f497e59c9
Jhon Lay Lopez <jhonlaylopez000@gmail.com>
2025-08-13 01:17:21 +0800

    feat(auth): implement user signup functionality

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,55 @@
+import bcrypt from "bcryptjs";
+
+import User from "../models/user.model.js";
+
+import { generateToken } from "../lib/utils.js";
+
+export const signup = async (req, res) => {
+	const { email, password } = req.body;
+
+	try {
+		if (!email || !password) {
+			return res
+				.status(400)
+				.json({ message: "Email and password are required" });
+		}
+
+		if (password.length < 6) {
+			return res
+				.status(400)
+				.json({ message: "Password must be at least 6 characters long" });
+		}
+
+		const user = await User.findOne({ email });
+
+		if (user) {
+			return res.status(400).json({ message: "User already exists" });
+		}
+
+		const salt = await bcrypt.genSalt(10);
+		const hashPassword = await bcrypt.hash(password, salt);
+
+		const newUser = new User({
+			email,
+			password: hashPassword,
+		});
+
+		if (newUser) {
+			generateToken(newUser._id, res);
+			await newUser.save();
+
+			return res.status(201).json({
+				user: {
+					id: newUser._id,
+					email: newUser.email,
+				},
+				message: "User created successfully",
+			});
+		} else {
+			return res.status(500).json({ message: "User creation failed" });
+		}
+	} catch (error) {
+		console.log(`Error in signup controller: ${error.message}`);
+		res.status(500).json({ message: "Internal server error" });
+	}
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,22 +1,29 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import connectDB from "./lib/db.js";
+
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 
 const app = express();
 
-// Middleware
-app.use(cors());
-app.use(express.json());
-
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-	res.send("Hello World!");
-});
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+	cors({
+		origin: "http://localhost:5173",
+		credentials: true,
+	})
+);
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
 	connectDB();
