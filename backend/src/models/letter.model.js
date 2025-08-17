07902369e93ffa1e4c16692d2beb42d8074aff12
Jhon Lay Lopez <jhonlaylopez000@gmail.com>
2025-08-17 22:33:52 +0800

    feat(letter): add letter model schema with userId, text, image, dateToBeOpened, and password fields

---
+++ backend/src/models/letter.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const letterSchema = new mongoose.Schema(
+	{
+		userId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+		text: {
+			type: String,
+			required: true,
+		},
+		image: {
+			type: [String],
+		},
+		dateToBeOpened: {
+			type: Date,
+			required: true,
+		},
+		password: {
+			type: String,
+		},
+	},
+	{ timestamps: true }
+);
